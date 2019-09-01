bd6d3517e1ce0d1656e8e3a72b5f4e7260e5fa31
Fábio Lamin <fabio_lamin@hotmail.com>
2019-09-01 11:02:14 -0300

    Creating Platform and PlatformManager scripts

---
+++ Infinity Urban/Assets/Scripts/Platform/Platform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private PlatformManager platformManager;
+    private bool isCounting = false;
+    private float timer = 0.35f;
+    void Start()
+    {
+        GameObject gameObject = GameObject.FindGameObjectWithTag("PlatformManager");
+        platformManager = gameObject.GetComponent<PlatformManager>();
+    }
+
+    private void Update()
+    {
+        if(isCounting)
+        {
+            Countdown();
+        }
+    }
+
+    void Countdown()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            platformManager.RecyclePlatform(this.gameObject);
+            timer = 0.35f;
+            isCounting = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isCounting = true;
+        }
+    }
+}
---
+++ Infinity Urban/Assets/Scripts/Platform/PlatformManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public GameObject[] platforms;
+    private float offSet = 0;
+
+    private void Start()
+    {
+        for(int x = 0; x < 6; x++)
+        {
+            InstantiatePlatform();
+        }
+    }
+
+    public void  InstantiatePlatform()
+    {
+        int index = Random.Range(0,2);
+        Instantiate(platforms[index], Vector3.forward * offSet, Quaternion.identity);
+        offSet += 11.9f;
+    }
+
+    public void RecyclePlatform(GameObject platform)
+    {
+        platform.transform.position = Vector3.forward * offSet;
+        offSet += 11.9f;
+    }
+}
