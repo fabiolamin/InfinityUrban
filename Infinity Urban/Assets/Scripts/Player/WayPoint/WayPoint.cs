ddc8c46c71e718b6ebb9e0e1699310314a8db8a7
Fábio Lamin <fabio_lamin@hotmail.com>
2019-09-07 09:58:24 -0300

    Creating WayPoint script

---
+++ Infinity Urban/Assets/Scripts/Player/WayPoint/WayPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void LateUpdate()
+    {
+        FollowTargetByZ();
+    }
+
+    public void FollowTargetByZ()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, target.position.z);
+    }
+}
