5dcc6e7fc7aa43e177f6607c7190b5026074f0da
Fábio Lamin <fabio_lamin@hotmail.com>
2019-09-08 11:13:00 -0300

    Creating Player and TouchInput scripts

---
+++ Infinity Urban/Assets/Scripts/Input/TouchInput.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+    private Vector3 fp;
+    private Vector3 lp;
+    private float dragDistance;
+ 
+    void Start()
+    {
+        dragDistance = Screen.height * 15 / 100;
+    }
+ 
+    public int Swipe()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                lp = touch.position;
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    { 
+                        if ((lp.x > fp.x)) 
+                        {   //Right swipe
+                            return 1;
+                        }
+                        else
+                        {   //Left swipe
+                            return -1;
+                        }
+                    }
+                    else
+                    {   
+                        if (lp.y > fp.y)  
+                        {
+                            return 2;
+                        }
+                        else
+                        {
+                            return -2;
+                        }
+                    }
+                }
+                else
+                {
+                    //Tap
+                }
+            }
+        }
+        return 0;
+    }
+}
---
+++ Infinity Urban/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2f;
+    TouchInput swipeInput;
+    Transform wayPoint;
+    float posX;
+    void Awake()
+    {
+        StartCoroutine(IncreaseSpeed());
+        wayPoint = this.transform;
+        swipeInput = GetComponent<TouchInput>();
+    }
+    void Update()
+    {
+        GetInput();
+        Run();
+        CalculateDirection();
+    }
+    public void Run()
+    {
+        transform.position += new Vector3(posX, 0, 1) * speed * Time.deltaTime;
+    }
+    public void CalculateDirection()
+    {
+        Vector3 direction = (wayPoint.position - transform.position).normalized;
+        posX = direction.x;
+    }
+    private void GetInput()
+    {
+        if (swipeInput.Swipe() == -1)
+        {
+            VerifyDirection(Vector3.left);
+        }
+
+        if (swipeInput.Swipe() == 1)
+        {
+            VerifyDirection(Vector3.right);
+        }
+    }
+
+    private void VerifyDirection(Vector3 direction)
+    {
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray.origin, ray.direction, out hit))
+        {
+            if (hit.collider.gameObject.CompareTag("WayPoint"))
+            {
+                wayPoint = hit.collider.gameObject.transform;
+            }
+        }
+    }
+
+    IEnumerator IncreaseSpeed()
+    {
+        while (speed < 15)
+        {
+            yield return new WaitForSeconds(4);
+            speed++;
+        }
+    }
+}
