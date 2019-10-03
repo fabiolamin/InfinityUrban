b3019883b558bc533866ac1b467d9e94df652468
Fábio Lamin <fabio_lamin@hotmail.com>
2019-10-03 09:44:15 -0300

    Adjusting the main camera

---
+++ Infinity Urban/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Vector3 offset;
+    private Vector3 desiredPosition;
+
+    private void Awake()
+    {
+        offset = transform.position - target.position;
+    }
+    void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, desiredPosition.z), 1f);
+    }
+}
